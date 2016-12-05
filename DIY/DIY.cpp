03be80c66b94179cbe22c6506c5f9a3c4649d0f5
Dimitri Mestdag <dimitri333@hotmail.fr>
2016-12-05 22:31:29 +0100

    MAJ 1.2 ?
    Le choix oui/non fonctionne mais il y a une erreur dans le dosage final ...

--- DIY/DIY.cpp
+++ DIY/DIY.cpp
@@ -1,38 +1,46 @@
-// DIY.cpp : définit le point d'entrée pour l'application console.
+// DIY.cppÂ : dÃ©finit le point d'entrÃ©e pour l'application console.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+
 using namespace std;
 
 int main()
 {
 	double recipient, totalbase, totalconcentre, base, concentre;
-	base = 85. / 100.;
-	concentre = 15. / 100.;
-	char reponce, oui ='o';
+	base = 90. / 100.;
+	concentre = 10. / 100.;
+	char reponce, oui='o',non='n';
+	//string oui = 'oui';
 	cout << "Entrer la capacite de votre recipient (en mL) : ";
 	cin >> recipient;
-	cout << "Voulez vous choisir le pourcentage d'arome ? (o/n) : ";
-	cin >> reponce;
-	if (reponce == oui)
-	{
-		cout << "Entrer le pourcentage d'arome voulu : ";
-		cin >> concentre;
-		concentre = concentre / 100;
-	}
-	else
-	{
-		cout << "Calcul par defaut avec 85% de base et 15% de concentre ..." << endl;
-	}
+
+	//do
+	//{
+		cout << "Voulez vous choisir le pourcentage d'arome ? (oui/non) : ";
+		cin >> reponce;
+		if (reponce == oui)
+		{
+			cout << "Entrer le pourcentage d'arome voulu : ";
+			cin >> concentre;
+			concentre = concentre / 100;
+		}
+		else if (reponce == non)
+		{
+			cout << "Calcul par defaut avec " << base * 100 << "% de base et " << concentre * 100 << " % de concentre ..." << endl;
+		}
+		else
+			cout << "Veuillez entrer oui ou non ";
+	//}
+	//while (reponce == oui || reponce == non);
+	totalbase = recipient*base;
 	totalconcentre = recipient*concentre;
-	totalbase = recipient-totalconcentre;
 
-	cout << "Dans votre recipient de " << recipient << "mL, vous devrez mettre " << totalbase << " mL de base et " << totalconcentre << " mL de concentre" <<endl;
-	system("PAUSE");
+	cout << "Dans votre recipient de " << recipient << "mL, vous devrez mettre " << totalbase << " mL de base et " << totalconcentre << " mL de concentre" << endl;
+	//system("PAUSE");
 	return 0;
-	
-}
 
+}
