670c74a6f0f585d485be70728f83779af6d396cb
Dimitri Mestdag <dimitri333@hotmail.fr>
2016-12-06 14:18:55 +0100

    Update DIY.cpp

--- DIY/DIY.cpp
+++ DIY/DIY.cpp
@@ -1,10 +1,5 @@
-// DIY.cpp : définit le point d'entrée pour l'application console.
-//
-
 //#include "stdafx.h"
 #include <iostream>
-//#include <stdlib.h>
-//#include <stdio.h>
 
 using namespace std;
 
@@ -13,37 +8,65 @@ int main()
 	double recipient, totalbase, totalconcentre, base, concentre;
 	base = 80. / 100.;
 	concentre = 20. / 100.;
-	//char reponse oui='o',non='n';
-	string reponse, oui = "oui", non="non";
+	string reponse, reponse2, oui = "oui", non = "non";
+
+	do
+	{
 	cout << "Entrer la capacite de votre recipient (en mL) : ";
 	cin >> recipient;
-
-	//do
-	//{
+	//system("clear");
+	}
+	while (recipient<=0);
+	do
+	{
 		cout << "Voulez vous choisir le pourcentage d'arome ? (oui/non) : ";
 		cin >> reponse;
-		if (reponse == oui)
-		{
-			cout << "Entrer le pourcentage d'arome voulu : ";
-			cin >> concentre;
-			base = 100-concentre;
-            concentre /= 100;
-            base/=100;
+		//system("clear");
+	}
+	while (reponse != oui && reponse != non);
 
+	if (reponse == oui)
+	{
+		cout << "Entrer le pourcentage d'arome voulu : ";
+		cin >> concentre;
+		//system("clear");
+		base = 100-concentre;
+		concentre /= 100;
+		base/=100;
+	}
+	else if (reponse == non)
+	{
+		
+		cout << "Connaissez vous la marque du concentré ? (oui/non) : ";
+		cin>>reponse2;
+		//system("clear");
+		do
+		{
+		if (reponse2==oui)
+		{
+			cout<<"Voici la liste des marques connues :"<<endl
+				<<"1. Red Aster"<<endl
+				<<"2. VDLV"<<endl;
+				
 		}
-		else if (reponse == non)
+		else if (reponse2==non)
 		{
 			cout << "Calcul par defaut avec " << base * 100 << "% de base et " << concentre * 100 << " % de concentre ..." << endl;
 		}
 		else
-			cout << "Veuillez entrer oui ou non ";
-	//}
-	//while (reponse == oui || reponse == non);
+		cout << "Veuillez entrer oui ou non ";
+		}
+		while (reponse != oui && reponse != non);
+	}
+
+	else
+	cout << "Veuillez entrer oui ou non ";
+	
 	totalbase = recipient*base;
 	totalconcentre = recipient*concentre;
 
 	cout << "Dans votre recipient de " << recipient << "mL, vous devrez mettre " << totalbase << " mL de base et " << totalconcentre << " mL de concentre" << endl;
 	//system("PAUSE");
 	return 0;
-
+	
 }
